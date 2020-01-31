fe90e68f3e470c8dca2c3aad14ccac497d428a47
Artem Fedulov <arfedulov@gmail.com>
2020-01-31 18:26:21 +0300

    add TaskList component

---
+++ src/views/TaskList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <task-filter></task-filter>
+    <ul>
+      <li v-for="task of tasks" :key="task.id">
+        <task :id="task.id" :title="task.title" :content="task.content"></task>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Task from '@/components/Task.vue';
+import TaskFilter from '@/components/TaskFilter.vue';
+
+export default {
+  name: 'task-list',
+  components: {
+    task: Task,
+    'task-filter': TaskFilter,
+  },
+  // TODO: add methods for dispatching store actions
+};
+</script>
